2ff8f815a9b94e3ad07903f5fe31c1cef622667d
jihun <gn753@naver.com>
2024-04-06 23:10:59 +0900

    fix: 로그인 CORS 이슈 => SSL 인증제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
 import tsconfigPaths from 'vite-tsconfig-paths'
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 // import envCompatible from "vite-plugin-env-compatible";
 
 export default defineConfig(({command, mode}) => {
   if (command === 'serve' && mode === 'development') {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())}
     return {
       env: env,
       plugins: plugins,
-      server: derServer,
+      server: {
+        open: '/',
+        proxy: {
+          // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
+          '/api': {
+            // 요청 전달 대상 서버 주소 설정
+            target: process.env.VITE_API_URL,
+            // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
+            changeOrigin: true,
+            // 요청 경로에서 '/api' 제거
+            rewrite: path => path.replace(/^\/api/, ''),
+            // SSL 인증서 검증 무시
+            secure: false,
+          },
+        },
+      },
     }
   } else {
     // command === 'build'
@@ -24,26 +40,7 @@ export default defineConfig(({command, mode}) => {
 const env = {
   // `.env` 파일의 모든 환경변수를 읽어옵니다.
   // `.env` 파일의 환경변수를 선택적으로 읽어오려면 `keys` 옵션을 사용합니다.
-  keys: ['VITE_API_KEY'],
-}
-
-const derServer = {
-  open: '/',
-  proxy: {
-    // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
-    '/api': {
-      // 요청 전달 대상 서버 주소 설정
-      target: 'https://perfume.app.cd80.run/api/v1',
-      // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
-      changeOrigin: true,
-      // 요청 경로에서 '/api' 제거
-      rewrite: path => path.replace(/^\/api/, ''),
-      // SSL 인증서 검증 무시
-      secure: true,
-      // WebSocket 프로토콜 사용
-      ws: true,
-    },
-  },
+  keys: ['VITE_API_URL', 'VITE_API_KEY'],
 }
 
 const plugins = [
